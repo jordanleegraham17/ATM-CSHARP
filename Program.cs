3f7bca8feba9b9bd4bd35d99719db6b7f612ec77
jordanleegraham17 <jordanleegraham17@outlook.com>
2024-01-15 20:27:07 +0000

    Created bankUser class with necessary getters and setters w/ constructors

--- Program.cs
+++ Program.cs
@@ -1,10 +1,73 @@
 ﻿using System;
 using System.Diagnostics;
 
-class Program
+public class bankUser
 {
-    static void Main()
+    String firstName;
+    String lastName;
+    String cardNumber;
+    int pin;
+    double balance;
+
+    //create bankUser constructor
+    public bankUser(string firstName, string lastName, string cardNumber, int pin, double balance)
     {
-        Console.WriteLine("ATM");
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.cardNumber = cardNumber;
+        this.pin = pin;
+        this.balance = balance;
     }
+
+    //getters and setters
+    public string getFirstName()
+    {
+        return firstName;
+    }
+
+    public void setFirstName(string firstName)
+    {
+        this.firstName = firstName;
+    }
+
+    public string getLastName()
+    {
+        return lastName;
+    }
+
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+    public String getCardNumber()
+    {
+        return cardNumber;
+    }
+
+    public void setCardNumber(String cardNumber)
+    {
+        this.cardNumber= cardNumber;
+    }
+
+    public int getPin()
+    {
+        return pin;
+    }
+
+    public void setPin(int pin)
+    {
+        this.pin = pin;
+    }
+
+    public double getBalance()
+    {
+        return balance;
+    }
+
+    public void setBalance(double balance)
+    {
+        this.balance = balance;
+    }
+
 }
